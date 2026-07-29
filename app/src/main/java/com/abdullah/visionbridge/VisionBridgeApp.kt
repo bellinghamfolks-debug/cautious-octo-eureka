dbc3c002ed0b718b898048a3d78365ca732467ee
bellinghamfolks-debug <bellinghamfolks@gmail.com>
2026-07-29 07:01:07 +0300

    Initialize persistent diagnostics

--- app/src/main/java/com/abdullah/visionbridge/VisionBridgeApp.kt
+++ app/src/main/java/com/abdullah/visionbridge/VisionBridgeApp.kt
@@ -1,8 +1,14 @@
 package com.abdullah.visionbridge
 
 import android.app.Application
+import com.abdullah.visionbridge.data.diagnostics.DiagnosticsHub
 import com.abdullah.visionbridge.di.AppContainer
 
 class VisionBridgeApp : Application() {
     val container: AppContainer by lazy { AppContainer(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        DiagnosticsHub.initialize(container.diagnostics)
+    }
 }
